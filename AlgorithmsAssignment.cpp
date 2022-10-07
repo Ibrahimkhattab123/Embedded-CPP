8dcb63cc46f3bc12170b35cef141a6a9e8ea28b4
ibrahimkhattab123 <ibrahimkhattab456@gmail.com>
2022-10-07 02:13:42 +0200

    creat README File and main File

---
+++ AlgorithmsAssignment.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <functional>
+
+// 1)
+// * Given a sorted Array in ascending order need to square every element in this array
+// * and maintain the sorting in the same order.
+// * {1,5,6,7,8,9}
+// * {1,25,36,49,64,81}
+// * Space complexity should be O(1)
+
+int main()
+{
+
+    return 0;
+}
