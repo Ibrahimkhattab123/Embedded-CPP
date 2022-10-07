c666d46d2242f61e089e50c641d4013ba9bec9ec
ibrahimkhattab123 <ibrahimkhattab456@gmail.com>
2022-10-07 16:37:11 +0200

    now done

--- AlgorithmsAssignment.cpp
+++ AlgorithmsAssignment.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <functional>
+#include "IntegerArray.h"
 
 // 1)
 // * Given a sorted Array in ascending order need to square every element in this array
@@ -8,8 +7,27 @@
 // * {1,25,36,49,64,81}
 // * Space complexity should be O(1)
 
+// 2)
+// * Given an array has Integer values and include multiple values of the same element 
+// * need to move the a given integer value to the end of the array.
+// * example 1,2,3,2,10,9,2,27,30,17,8,9    (repeated values: 2, 9 )
+// * answer  1 3 10 9 27 30 17 8 9 2 2 2 
+// * Time complexity should be O ( n ) << don't worry if u dont know what is it
+
 int main()
 {
+    
+     // 1)
 
+    integer_array<int, bool> FirstArray(1);
+    FirstArray.Square_Element();
+    FirstArray.Print_Elements();
+
+    // 2)
+
+    integer_array<int, bool> SecondArray(0);
+    SecondArray.Pushback_Given_Element(2);
+    SecondArray.Print_Elements();
+    
     return 0;
 }
